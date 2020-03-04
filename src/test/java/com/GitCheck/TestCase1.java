5b737c7a85f9fcf2b761c69224ca2704b538cd58
Madan <Madan@DESKTOP-F0AMVN3>
2020-03-05 00:27:09 +0530

    creating repo in git hub

---
+++ src/test/java/com/GitCheck/TestCase1.java
@@ -0,0 +1,21 @@
+package com.GitCheck;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	WebDriver driver;
+	@Test
+	public void method1(){
+		
+		System.out.println("1");
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Madan\\Documents\\New folder\\GitCheck\\chromedriver.exe");
+		 driver =new ChromeDriver();
+	        String baseurl = "http://www.google.com/";
+	        driver.get(baseurl);
+	    }
+	}
+	
+
